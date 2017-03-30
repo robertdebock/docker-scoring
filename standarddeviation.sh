c41506e00c6f7654d3ae5c52ba2391ecd34fa23a
Robert de Bock <robert@meinit.nl>
2017-03-30 16:24:49 +0200

    Update standarddeviation.sh

--- standarddeviation.sh
+++ standarddeviation.sh
@@ -2,5 +2,7 @@
 
 cat /data/input.txt | while read technology scores ; do
   printf "${technology} "
-  echo "${scores}" | tr ' ' '\n' |  awk '{sum+=$1; sumsq+=$1*$1}END{print sqrt(sumsq/NR - (sum/NR)**2)}'
+  echo "${scores}" | awk '{ A=0; V=0; for(N=1; N<=NF; N++) A+=$N ; A/=NF ; for(N=1; N<=NF; N++) V+=(($N-A)*($N-A))/(NF-1); print sqrt(V) }' | while read output ; do
+    echo "scale=1; ${output}/1" | bc
+  done
 done
